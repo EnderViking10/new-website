c0002a3e0c96475fc0798da2832cd5db45a2674e
Enderviking19 <mrerikk6@gmail.com>
2020-01-29 21:07:21 -0500

    Update main.js

--- Clicker/main.js
+++ Clicker/main.js
@@ -154,7 +154,7 @@ function checkCookiess(){
 	for(let i=0; i<9; i++){
 	  // Num cookies
 		console.log("x");
-		ncookie[i] = getCookie("num" + i);
+		ncookie[i] = getCookie("num" + toString(i));
 		console.log("x");
 		if(ncookie[i] != undefined)
 			num[i] = ncookie[i];
