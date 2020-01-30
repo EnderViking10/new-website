ef22725ded2ace1dd8f9122ce81b0e02805d2ccf
Enderviking19 <mrerikk6@gmail.com>
2020-01-29 21:05:32 -0500

    Update main.js

--- Clicker/main.js
+++ Clicker/main.js
@@ -69,8 +69,7 @@ checkCookiess();
 function run(){
 	for(let i=0; i<10; i++){
 				
-		setCookie("num" + i, num[i], 365);
-		setCookie("inc" + i, inc[i], 365);
+		
 		
 		bar[i] = document.getElementById("bar" + (i+1));
 
